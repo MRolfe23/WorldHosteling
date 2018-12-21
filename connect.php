7a239008b631950aa59f1131b792da85fb192dd8
MRolfe23 <31248765+MRolfe23@users.noreply.github.com>
2018-12-21 11:57:35 -0600

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,14 +1,15 @@
 <?php
-//$ini = parse_ini_file('~/worldhosteling.ini',true);
+$ini = parse_ini_file('~/worldhosteling.ini',true);
 /* Database config*/
-/*$db_host		=$ini['hostedDB']['userIP'];
-$db_user		=$ini['hostedDB']['userID'];
-$db_pass		=$ini['hostedDB']['userPass'];
-$db_database	=$ini['hostedDB']['userDB'];*/
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_database = "finalproject";
+$db_host		=$ini['hostedDB']['ip'];
+$db_user		=$ini['hostedDB']['user'];
+$db_pass		=$ini['hostedDB']['pass'];
+$db_database	=$ini['hostedDB']['db'];
+
+$db_host = "$db_host";
+$db_user = "$db_user";
+$db_pass = "$db_pass";
+$db_database = "$db_database";
 /* End Config*/
 $db = new PDO('mysql:host='.$db_host.'; dbname='.$db_database, $db_user, $db_pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
